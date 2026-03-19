3f002b8a5fa350c9287a5369b2b74a30c5e09bfd
Moshe-shmuel <MS123123@outlook.co.il>
2026-03-19 10:58:06 +0200

    ניסיון נוסףףףף

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,7 +10,7 @@ fn get_resource_path(app: tauri::AppHandle) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_sql::init()) 
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_path::init())
         .invoke_handler(tauri::generate_handler![get_resource_path])
